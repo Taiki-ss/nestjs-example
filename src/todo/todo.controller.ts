75c3c1c46dc40a48aa15566adc721b8f577fdcde
Taiki-nk <taikifate.0206@gmail.com>
2023-01-15 12:20:03 +0900

    sanple TODO list作成

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  @Get('list')
+  getList() {
+    return [{ title: 'NestJSを学ぶ', due_on: '2023-1-15', done: false }];
+  }
+}
