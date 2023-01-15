87ac8f35313d5d9ff902a1931454fc39458bf272
Taiki-nk <taikifate.0206@gmail.com>
2023-01-15 22:43:52 +0900

    validation実装

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTaskDto } from './create-task.dto';
+import { UpdateTaskDto } from './update-task.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -8,13 +9,30 @@ export class TodoController {
 
   @Get('list')
   async getList() {
-    const result = await this.prisma.task.findMany();
+    const result = await this.prisma.task.findMany({
+      where: {
+        is_done: false,
+      },
+    });
     return [...result];
   }
 
   @Post('')
   async add(@Body() task: CreateTaskDto) {
     const result = await this.prisma.task.create({ data: task });
+    return { status: 'OK' };
+  }
+
+  @Post(':id/done')
+  async done(@Param() param: UpdateTaskDto) {
+    await this.prisma.task.update({
+      data: {
+        is_done: true,
+      },
+      where: {
+        id: param.id,
+      },
+    });
     return { status: 'OK' };
   }
 }
